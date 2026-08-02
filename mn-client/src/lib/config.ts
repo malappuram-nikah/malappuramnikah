5cf4535184b0e1f1e9480ea5ea40a21f9fb6b31e
haris700 <harisvkvnr@gmail.com>
2026-08-02 13:31:52 +0530

    Support VITE_REACT_APP_API_URL and NEXT_PUBLIC_API_URL in config.ts

--- mn-client/src/lib/config.ts
+++ mn-client/src/lib/config.ts
@@ -1,1 +1,6 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333";
+export const API_URL = 
+  process.env.NEXT_PUBLIC_API_URL || 
+  process.env.VITE_REACT_APP_API_URL || 
+  process.env.REACT_APP_API_URL || 
+  "http://localhost:3333";
+
