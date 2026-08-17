c29317ca2418ae5c70eb1259befaea000e835f8b
haris700 <harisvkvnr@gmail.com>
2026-08-17 09:58:08 +0530

    chore: update API_URL configuration and .env settings

--- mn-client/src/lib/config.ts
+++ mn-client/src/lib/config.ts
@@ -1,6 +1,5 @@
 export const API_URL = 
   process.env.NEXT_PUBLIC_API_URL || 
-  process.env.VITE_REACT_APP_API_URL || 
   process.env.REACT_APP_API_URL || 
   "https://malappuramnikah.onrender.com";
 
